72537c55de96f4b7004155418f902dfcae2cd322
Adrian Fernandez Murias <adrianfernandezmurias@gmail.com>
2026-03-25 13:43:34 +0100

    Creacion vistas Base de datos

--- Base de datos/ProyectoHappiness.sql
+++ Base de datos/ProyectoHappiness.sql
@@ -1,5 +1,3 @@
-drop schema happinessandco;
-
 /* 1. CREACIÓN DE LA BASE DE DATOS*/
 CREATE SCHEMA HappinessAndCo;
 USE HappinessAndCo;
@@ -54,17 +52,12 @@ INSERT INTO Eventos (id, fecha, titulo, ubicacion, descripcion) VALUES
 (5, '2026-06-15', 'Exposición de Arte', 'Bilbao', 'Galería de arte moderno.'),
 (6, '2026-06-25', 'Maratón Solidario', 'Zaragoza', 'Carrera benéfica.');
 
-select*
-from eventos;
-
 -- Insertar 3 Usuarios
 INSERT INTO Usuarios (id, nombre, email, password) VALUES
 (1, 'Ana García', 'ana@outlook.es', '123456'),
 (2, 'Luis Pérez', 'luis@hotmail.com', 'Hola1234'),
 (3, 'María López', 'maria@gmail.com', 'Caracola22');
 
-select*
-from usuarios;
 
 -- Insertar 3 Galerías 
 INSERT INTO Galerias (id, titulo, id_evento) VALUES
@@ -72,8 +65,6 @@ INSERT INTO Galerias (id, titulo, id_evento) VALUES
 (2, 'Imágenes del Congreso Tech', 2),
 (3, 'Momentos Feria Gastronómica', 3);
 
-select*
-from galerias;
 
 -- Insertar Imágenes Galerias
 INSERT INTO Imagenes_galerias (id, titulo, imagen, id_galeria) VALUES
@@ -89,3 +80,47 @@ INSERT INTO Imagenes_galerias (id, titulo, imagen, id_galeria) VALUES
 (301, 'Plato estrella', 'plato.jpg', 3),
 (302, 'Chef invitado', 'chef.jpg', 3),
 (303, 'Postres', 'postres.jpg', 3);
+
+
+-- Favoritos: 3 por usuario
+INSERT INTO Favoritos (id_usuario, id_evento) VALUES
+-- Usuario 1: 
+(1, 1), (1, 2), (1, 4),
+-- Usuario 2:
+(2, 2), (2, 3), (2, 5),
+-- Usuario 3:
+(3, 1), (3, 3), (3, 6);
+
+
+/* 4.CREACION VISTAS*/
+
+-- Devuelvan las galerías anteriores al 28-02-2026. 
+CREATE VIEW Galerias_Anteriores_Febrero AS
+SELECT Galerias.id, Galerias.titulo, Galerias.id_evento, Eventos.fecha
+FROM Galerias
+JOIN Eventos ON Galerias.id_evento = Eventos.id
+WHERE Eventos.fecha < '2026-02-28';
+
+
+-- Devuelvan los eventos favoritos del usuario 1.  
+CREATE VIEW Favoritos_Usuario_1 AS
+SELECT Eventos.id, Eventos.fecha, Eventos.titulo, Eventos.ubicacion, Eventos.descripcion
+FROM Eventos
+JOIN Favoritos ON Eventos.id = Favoritos.id_evento
+WHERE Favoritos.id_usuario = 1;
+
+
+-- Devuelvan las imagenes de la galeria del evento del 12-01-2026.
+CREATE VIEW Imagenes_Galeria_Evento AS
+SELECT Imagenes_galerias.id, Imagenes_galerias.titulo, Imagenes_galerias.imagen, Imagenes_galerias.id_galeria
+FROM Imagenes_galerias
+JOIN Galerias ON Imagenes_galerias.id_galeria = Galerias.id
+WHERE Galerias.id_evento = 2;
+
+
+-- Devuelvan los eventos favoritos del usuario 2 posteriores al 28-02-2026. 
+CREATE VIEW Eventos_Favoritos_Usuario_2 AS
+SELECT Eventos.id, Eventos.fecha, Eventos.titulo, Eventos.ubicacion, Eventos.descripcion
+FROM Eventos
+JOIN Favoritos ON Eventos.id = Favoritos.id_evento
+WHERE Favoritos.id_usuario = 2 AND Eventos.fecha > '2026-02-28';
